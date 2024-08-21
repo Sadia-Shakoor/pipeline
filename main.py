62df2f7e37f4318abe5b9b5e2e86cf5b0d8b8c85
Sadia-Shakoor <sp23-bai-047@cuilahore.edu.pk>
2024-08-21 19:56:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ def read_from_gcs(bucket_name, file_name):
     content = blob.download_as_text()
     return content
 
-def read_from_sql():
+#def read_from_sql():
     """Reads data from Cloud SQL."""
-    connection = pymysql.connect(
+    """connection = pymysql.connect(
         user=DB_USER,
         password=DB_PASSWORD,
         database=DB_NAME,
@@ -43,12 +43,12 @@ def read_from_sql():
         rows = cursor.fetchall()
 
     connection.close()
-    return rows
+    return rows"""
 
-def transform_to_json(sql_data, json_data, csv_data):
+def transform_to_json("""sql_data,""" json_data, csv_data):
     """Transforms SQL, JSON, and CSV data into a single JSON object."""
     combined_data = {
-        'sql_data': sql_data,
+        #'sql_data': sql_data,
         'json_data': json.loads(json_data),
         'csv_data': list(csv.DictReader(csv_data.splitlines()))
     }
@@ -69,11 +69,11 @@ def run_pipeline():
     csv_data = read_from_gcs(GCS_BUCKET_NAME, GCS_CSV_FILE_NAME)
 
     # Read data from Cloud SQL
-    sql_data = read_from_sql()
-    print(sql_data)
+    #sql_data = read_from_sql()
+    #print(sql_data)
 
     # Transform data
-    transformed_data = transform_to_json(sql_data, json_data, csv_data)
+    transformed_data = transform_to_json( json_data, csv_data)
 
     # Upload transformed data to GCS
     upload_to_gcs(GCS_BUCKET_NAME, GCS_OUTPUT_JSON_FILE_NAME, transformed_data)
